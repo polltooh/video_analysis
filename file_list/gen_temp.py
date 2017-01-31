4885ebca37de4b31a31e84899c5603ff7482a9aa
polltooh <polltooh.gmail>
2017-01-31 14:47:51 +0800

    clean dir

--- file_list/gen_temp.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-a = np.zeros((256, 256), np.float32)
-a.tofile("temp1.npy")
-
-b = np.ones((256, 256), np.float32)
-b.tofile("temp2.npy")
--- file_list/rgb2grey.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-i = cv2.imread("traffic.jpg")
-i_grep = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
-i_grep = cv2.resize(i_grep, (256, 256))
-i_grep = i_grep.astype(np.float32)
-i_grep.tofile("traffic_gray.npy")
-print(i_grep)
