c6b18e3c0c06c2a7bbc1226da9e12a1e805c7715
polltooh <polltooh@gmail.com>
2017-02-25 23:30:03 +0800

    clean repo

--- nn_script/model_exc.py
+++
@@ -1,9 +0,0 @@
-from vgg_model import Model
-from traffic_data_ph import DataPh
-
-
-def model_exc(is_train):
-    infer = Model.model_infer(DataPh)
-    loss = Model.model_loss(infer, DataPh)
-    if is_train:
-        Model.model_mini(loss)
