35cec80281fbcb8a5bdcf0078803a5b441c31fc0
polltooh <polltooh@gmail.com>
2017-03-05 21:11:00 +0800

    add cal results

---
+++ results/analize_result.py
@@ -0,0 +1,22 @@
+from TensorflowToolbox.utility import file_io
+import sys
+import numpy as np
+
+def mae(label_list, infer_list):
+    mae_v = np.mean(np.abs(label_list - infer_list))
+    return mae_v
+
+if __name__ == "__main__":
+    #file_name = sys.argv[1] 
+    file_name = "trancos_result1.txt"
+    file_list = file_io.read_file(file_name)
+    label_list = list()
+    infer_list = list()
+
+    for f in file_list:
+        n, label, infer = f.split(" ")
+        label_list.append(float(label)) 
+        infer_list.append(float(infer))
+
+    mae_v = mae(np.array(label_list), np.array(infer_list))
+    print(mae_v)
