2126940716bb23d11156b407103b5a8e63f002a2
polltooh <polltooh@gmail.com>
2017-03-06 18:28:09 +0800

    add check file number file

---
+++ data_process_script/ucsd_file_check.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+from TensorflowToolbox.utility import file_io
+
+desmap_dir = "gtDensities/"
+
+desmap_list = file_io.get_listfile(desmap_dir, "desmap")
+new_list = list()
+for f in desmap_list:
+    num = np.sum(np.fromfile(f, np.float32))
+    format_num = "%.2f"%num
+    new_list.append(f + " " + format_num)
+    
+
+new_list.sort()
+file_io.save_file(new_list, "file_label_num.txt")
