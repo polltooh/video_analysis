58103e54d0140785d892c2918ed47a0848fdf64b
polltooh <polltooh@gmail.com>
2017-03-06 18:55:03 +0800

    add changes

--- data_process_script/ucsd_file_copy.py
+++ data_process_script/ucsd_file_copy.py
@@ -7,4 +7,6 @@ num_list = list()
 for i, f in enumerate(file_list):
     if i != 0 and i % 200 == 0 and i != 2000:
         i = i-1
-        shutil.copy2(file_list[i-1], file_list[i])        
+        f_pre = file_list[i-1].split(" ")[0]
+        f_curr = file_list[i].split(" ")[0]
+        shutil.copy2(f_pre, f_curr)        
