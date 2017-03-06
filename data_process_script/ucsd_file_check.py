5c8bafb6b83acaa445ed03367b652aceb5db8f10
polltooh <polltooh@gmail.com>
2017-03-06 05:52:42 -0500

    change path

--- data_process_script/ucsd_file_check.py
+++ data_process_script/ucsd_file_check.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from TensorflowToolbox.utility import file_io
 
-desmap_dir = "gtDensities/"
+desmap_dir = "/media/dog/data/UCSD/gtDensities/"
 
 desmap_list = file_io.get_listfile(desmap_dir, "desmap")
 new_list = list()
@@ -13,4 +13,4 @@ for f in desmap_list:
     
 
 new_list.sort()
-file_io.save_file(new_list, "file_label_num.txt")
+file_io.save_file(new_list, "ucsd_file_label_num.txt")
