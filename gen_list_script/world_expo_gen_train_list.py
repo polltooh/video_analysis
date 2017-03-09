95fe573ef01c2527fe4c675b192912e85341a4cc
polltooh <polltooh@gmail.com>
2017-03-09 08:50:31 -0500

    change path

--- gen_list_script/world_expo_gen_train_list.py
+++ gen_list_script/world_expo_gen_train_list.py
@@ -1,11 +1,12 @@
 from TensorflowToolbox.utility import file_io
 import numpy as np
+import os
 
 new_dsize = (256, 256)
 
 if __name__ == "__main__":
     label_path = "/media/dog/data/WorldExpo/train_label/"
-    mask_dir = "/media/dog/data/WorldExpo/train_mask/"
+    mask_dir = "/media/dog/data/WorldExpo/mask/"
     desmap_dir = "/media/dog/data/WorldExpo/gtDensity/gtDensity/"
     img_dir = "/media/dog/data/WorldExpo/train_frame/"
 
@@ -16,9 +17,11 @@ if __name__ == "__main__":
         if not img_name.endswith("_" + scale_str + ".jpg"):
             continue
         label_name = img_name.replace(img_dir, desmap_dir).replace(".jpg", ".npy")
-        mask_name = mask_dir + img_name.split("/")[-1].split("_")[0] \
-                    + "_" + scale_str + ".npy"
-        print(img_name)
+        mask_name = mask_dir + img_name.split("/")[-1].split("-")[0] \
+                    + "_mask_" + scale_str + ".npy"
         print(label_name)
         print(mask_name)
+        assert(os.path.exists(img_name))
+        assert(os.path.exists(label_name))
+        assert(os.path.exists(mask_name))
         exit(1)
