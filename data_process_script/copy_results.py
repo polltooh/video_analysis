330719a08a2eeb537032c821b5726bf727ab3a2c
polltooh <polltooh@gmail.com>
2017-03-14 19:23:45 +0800

    add copy results

---
+++ data_process_script/copy_results.py
@@ -0,0 +1,40 @@
+from TensorflowToolbox.utility import file_io
+import shutil
+import os
+
+def name_to_file_list(file_name):
+    file_list = file_io.read_file(file_name)
+    infer_list = list()
+    for i, f in enumerate(file_list):
+        f = f.split(" ")[0]
+        infer_name = f.replace(".jpg", ".infer_desmap")
+        if not os.path.exists(infer_name):
+            continue
+        infer_list.append(infer_name)
+        file_list[i] = f
+    
+    return file_list, infer_list
+
+def copyfile(file_list, src_path, des_path):
+    for f in file_list:
+        new_file_name = f.replace(src_path, des_path)
+        file_path = os.path.dirname(new_file_name)
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+
+        shutil.copy(f, new_file_name)
+
+src_path = "/media/dog/data/WebCamT_60000"
+des_path = "/media/dog/data/WebCamT_60000_desmap"
+
+downtown_test_file_name = \
+        "/home/shanghang/video_lstm/file_list/downtown_test_list1.txt"
+file_list, infer_list = name_to_file_list(downtown_test_file_name)
+copyfile(file_list, src_path, des_path)
+copyfile(infer_list, src_path, des_path)
+
+parkway_test_file_name = \
+        "/home/shanghang/video_lstm/file_list/parkway_test_list1.txt"
+file_list, infer_list = name_to_file_list(parkway_test_file_name)
+copyfile(file_list, src_path, des_path)
+copyfile(infer_list, src_path, des_path)
