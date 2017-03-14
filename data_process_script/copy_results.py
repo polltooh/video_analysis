c6cf54170734fdb4521111d88545c60d0367c1a0
polltooh <polltooh@gmail.com>
2017-03-14 20:07:13 +0800

    add mask

--- data_process_script/copy_results.py
+++ data_process_script/copy_results.py
@@ -5,15 +5,20 @@ import os
 def name_to_file_list(file_name):
     file_list = file_io.read_file(file_name)
     infer_list = list()
+    mask_list = list()
+
     for i, f in enumerate(file_list):
         f = f.split(" ")[0]
         infer_name = f.replace(".jpg", ".infer_desmap")
         if not os.path.exists(infer_name):
             continue
+        mask_name = "/".join(f.split("/")[:-1]) + "_msk_128.npy"
+
         infer_list.append(infer_name)
+        mask_list.append(mask_name)
         file_list[i] = f
     
-    return file_list, infer_list
+    return file_list, infer_list, mask_name
 
 def copyfile(file_list, src_path, des_path):
     for f in file_list:
@@ -29,12 +34,14 @@ des_path = "/media/dog/data/WebCamT_60000_desmap"
 
 downtown_test_file_name = \
         "/home/shanghang/video_lstm/file_list/downtown_test_list1.txt"
-file_list, infer_list = name_to_file_list(downtown_test_file_name)
+file_list, infer_list, mask_list = name_to_file_list(downtown_test_file_name)
 copyfile(file_list, src_path, des_path)
 copyfile(infer_list, src_path, des_path)
+copyfile(mask_list, src_path, des_path)
 
 parkway_test_file_name = \
         "/home/shanghang/video_lstm/file_list/parkway_test_list1.txt"
-file_list, infer_list = name_to_file_list(parkway_test_file_name)
+file_list, infer_list, mask_list  = name_to_file_list(parkway_test_file_name)
 copyfile(file_list, src_path, des_path)
 copyfile(infer_list, src_path, des_path)
+copyfile(mask_list, src_path, des_path)
